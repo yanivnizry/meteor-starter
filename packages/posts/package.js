cfc55f58b53deda0d09a78eb514619956b7795aa
Yaniv Nizry <yanivnizry@gmai.com>
2015-12-12 11:25:07 +0000

    daily build

---
+++ packages/posts/package.js
@@ -0,0 +1,49 @@
+Package.describe({
+  name: "posts",
+  summary: "Manage posts",
+  version: "0.1.0",
+});
+
+both = ['client','server']
+
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@1.2');
+
+  api.use(
+  	[
+  	  'app-lib',
+      'mongo',
+      'templating',
+	  'aldeed:autoform@4.1.0 || 5.1.2'
+  	],
+  	both);
+
+  api.addFiles(
+  	[
+  		'lib/both/collections.js'
+  	],
+  	both);
+
+   api.addFiles(
+  	[
+  		'lib/client/templates.html',
+      'lib/client/templates.js',
+      'lib/client/helpers.js',
+  		'lib/client/autoform.js'
+  	],
+  	'client');
+
+   api.addFiles(
+  	[
+  		'lib/server/allow.js',
+  		'lib/server/publish.js'
+  	],
+  	'server');
+   api.export('Comments',both);
+
+   api.imply('aldeed:simple-schema');
+   api.imply('aldeed:collection2');
+
+
+
+});
